6bad9421b30b135a87fd5af9076bc80346992a28
Mohammed Adel <mohammedadell301@gmail.com>
2023-05-31 14:11:58 +0300

    first commit

---
+++ app/Models/Orderitem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orderitem extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'product_id',
+        'quantity',
+        'quantity',
+        'price',
+        'order_id',
+    ];
+    public function product(){
+        return $this->belongsTo(Product::class,'product_id','id');
+    }
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+
+
+
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'image',
+        'trend',
+        'subcategory_id'
+    ];
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+    public function orderItems(){
+        return $this->hasMany(Orderitem::class);
+    }
+
+}
