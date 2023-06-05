c012565328b799e80cb72198b404fe646b1e11b2
nadahamed <nadahamed123455@gmail.com>
2023-06-05 14:22:50 +0300

    ...

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -27,7 +27,6 @@ class Product extends Model
         ];
     }
 
-
     public function subcategory()
     {
         return $this->belongsTo(Subcategory::class);
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PackageController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +21,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//--------packages-----------------------
+Route::apiResource('package',PackageController::class);
+
