e2edfdb8533de52ebe58d56248f0632b594af000
islam-saman <islam.saman4@gmail.com>
2023-06-11 15:07:31 +0300

    adding the discount column to the product table

--- database/migrations/2023_06_11_115941_add_discount_column_to_produt.php
+++ database/migrations/2023_06_11_115941_add_discount_column_to_produt.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('produt', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->string('discount')->after('price')->nullable();
 
         });
