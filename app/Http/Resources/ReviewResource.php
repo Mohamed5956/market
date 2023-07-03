fa7b3c9ffd581a1458c708578161b87d6adf676e
Mario-E-G <marioehab330@gmail.com>
2023-07-03 22:19:33 +0300

    updating....

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -20,6 +20,7 @@ class ReviewResource extends JsonResource
             'comment' => $this->comment,
             'user_id' => new UserResource(($this->whenLoaded('user'))),
             'product_id' => $this -> product_id,
+            'created_at' => $this->created_at,
         ];
     }
 }
